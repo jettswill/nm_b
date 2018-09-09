5ecc0236ac5b4b41c24877aeea164ac73d344236
Andrew Sergeev <jettswill@gmail.com>
2018-09-09 22:47:46 +0300

    Создана карточка клиента, подключена тема
    Доработана тема, создан новый паттерн pt-card-block

--- desktop.bundles/object/object.bemjson.js
+++ desktop.bundles/object/object.bemjson.js
@@ -10,127 +10,118 @@ module.exports = {
 		{ elem: 'js', url: 'https://code.jquery.com/jquery-3.2.1.js' },
 		  ],
 	scripts: [{ elem: 'js', url: 'object.min.js' }],
-    mods: { theme: 'nm' },
+    mods: { theme: 'nm-portal' },
 	mix: {
 			block: 'theme',
-			mods: { color: 'whitepaper-default',
+			mods: { color: 'nm-portal',
                     space: 'default',
                     size: 'default',
                     gap: 'medium',
                     menu: 'default',
-                    font: 'museo'
+                    font: 'tahoma'
 				  }
 		},
             content: [
                         {
-                            block: 'pt-card',
-                            mods: { shadow: 'cloud', view: 'default' },
+                            block: 'pt-card-block',
+                            mods: { shadow: 'card', view: 'default', 'border-radius':'xxs' },
                             content: [
-                                {
-                                elem: 'content',
-                                elemMods: { 'space-h': 'l', 'space-v': 'm'},
-                                content: [
-                                            {
-                                            block: 'pt-icon-plus',
-                                            mods: { 'vertical-align': 'center', 'distribute': 'between' },
+                                        {
+                                            elem: 'header',
+                                            elemMods: { height: 'm', distribute: 'between', border:'b', 'space-h':'l' },
                                             content: [
-                                                            {
-                                                                elem: 'block',
-                                                                mix: { block: 'text', mods: { size: 'm', view: 'brand', weight: 'bold' } },
-                                                                content: 'Информация о клиенте'
-                                                            },
-                                                            {
-                                                            elem: 'icon',
-                                                            elemMods: { 'indent-r': 'xs' },
-                                                            content: {
-                                                                block: 'icon',
-                                                                mods: { name: 'edit', size: 's', view: 'secondary' }
-                                                                    }
-                                                            }
-													]
-                                            }
-                                        ]
-                                },
-                                {
-                                elem: 'content',
-                                elemMods: { 'space-h': 'l', 'space-t': 'xs', 'space-b': 'xs'},
-                                content: [
-                                    {
-                                    block: 'pt-list',
-                                    content: [
-                                                 {
-                                        elem: 'item',
+                                                                        {
+                                                                            elem: 'block',
+                                                                            mix: { block: 'text', mods: { size: 'm', view: 'primary', weight: 'bold' } },
+                                                                            content: 'Информация о клиенте'
+                                                                        },
+                                                                        {
+                                                                            elem: 'icon',
+                                                                            elemMods: { 'indent-r': 'xs' },
+                                                                            content: {
+                                                                                block: 'icon',
+                                                                                mods: { name: 'edit', size: 's', view: 'primary'}
+                                                                                     }
+                                                                        }
+                                                     ]
+                                        },
+                                        {
+                                        elem: 'content',
+                                        elemMods: { 'space-h': 'l', 'space-t': 's', 'space-b': 'm'},
                                         content: [
                                             {
-                                                elem: 'item',
-                                                content: {
-                                                    block: 'pt-icon-plus',
-                                                    mods: { 'vertical-align': 'center' },
-                                                    content: [
-                                                    {
-                                                        elem: 'icon',
-                                                        elemMods: { 'indent-r': 'xs' },
+                                            block: 'pt-list',
+                                            content: [
+                                                        {
+                                                        elem: 'item',
+                                                        elemMods: {'space-b': 's'},
                                                         content: {
-                                                            block: 'icon',
-                                                            mods: { name: 'human', size: 'm', view: 'secondary' }
+                                                            block: 'pt-icon-plus',
+                                                            mods: { 'vertical-align': 'center' },
+                                                            content: [
+                                                            {
+                                                                elem: 'icon',
+                                                                elemMods: { 'indent-r': 'xs' },
+                                                                content: {
+                                                                    block: 'icon',
+                                                                    mods: { name: 'human', size: 'm', view: 'ghost' }
+                                                                }
+                                                            },
+                                                            {
+                                                                elem: 'text',
+                                                                mix: { block: 'text', mods: { size: 'm', view: 'primary' } },
+                                                                content: 'John Doe'
+                                                            }]
+                                                                }
+                                                        },
+                                                        {
+                                                            elem: 'item',
+                                                            elemMods: {'space-b': 's'},
+                                                            content: {
+                                                                block: 'pt-icon-plus',
+                                                                mods: { 'vertical-align': 'center' },
+                                                                content: [
+                                                                {
+                                                                    elem: 'icon',
+                                                                    elemMods: { 'indent-r': 'xs' },
+                                                                    content: {
+                                                                        block: 'icon',
+                                                                        mods: { name: 'phone', size: 'm', view: 'ghost' }
+                                                                    }
+                                                                },
+                                                                {
+                                                                    elem: 'text',
+                                                                    mix: { block: 'text', mods: { size: 'm', view: 'primary' } },
+                                                                    content: '+7 012 345-67-89'
+                                                                }]
+                                                            }
+                                                        },
+                                                        {
+                                                            elem: 'item',
+                                                            content: {
+                                                                block: 'pt-icon-plus',
+                                                                mods: { 'vertical-align': 'center' },
+                                                                content: [
+                                                                {
+                                                                    elem: 'icon',
+                                                                    elemMods: { 'indent-r': 'xs' },
+                                                                    content: {
+                                                                        block: 'icon',
+                                                                        mods: { name: 'mail', size: 'm', view: 'ghost' }
+                                                                    }
+                                                                },
+                                                                {
+                                                                    elem: 'text',
+                                                                    mix: { block: 'text', mods: { size: 'm', view: 'primary' } },
+                                                                    content: 'user@mail.com'
+                                                                }]
+                                                                    }
                                                         }
-                                                    },
-                                                    {
-                                                        elem: 'block',
-                                                        mix: { block: 'text', mods: { size: 'm', view: 'primary' } },
-                                                        content: 'John Doe'
-                                                    }]
-                                                }
-                                            },
-                                            {
-                                                elem: 'item',
-                                                content: {
-                                                    block: 'pt-icon-plus',
-                                                    mods: { 'vertical-align': 'center' },
-                                                    content: [
-                                                    {
-                                                        elem: 'icon',
-                                                        elemMods: { 'indent-r': 'xs' },
-                                                        content: {
-                                                            block: 'icon',
-                                                            mods: { name: 'phone', size: 'm', view: 'secondary' }
-                                                        }
-                                                    },
-                                                    {
-                                                        elem: 'block',
-                                                        mix: { block: 'text', mods: { size: 'm', view: 'primary' } },
-                                                        content: '+7 012 345-67-89'
-                                                    }]
-                                                }
-                                            },
-                                            {
-                                                elem: 'item',
-                                                elemMods: { 'space-v': 'xs' },
-                                                content: {
-                                                    block: 'pt-icon-plus',
-                                                    mods: { 'vertical-align': 'center' },
-                                                    content: [
-                                                    {
-                                                        elem: 'icon',
-                                                        elemMods: { 'indent-r': 'xs' },
-                                                        content: {
-                                                            block: 'icon',
-                                                            mods: { name: 'mail', size: 'm', view: 'secondary' }
-                                                        }
-                                                    },
-                                                    {
-                                                        elem: 'block',
-                                                        mix: { block: 'text', mods: { size: 'm', view: 'primary' } },
-                                                        content: 'user@mail.com'
-                                                    }]
-                                            }
-                                        }]
-                                    }
-                                             ]
-                                         }
-                                        ]
+                                                     ]
+                                                 }
+                                            ]
+                                        }
+                                    ]
                                 }
-                            ]
-                        }
-            		 ]
+            		        ]
 			 };
